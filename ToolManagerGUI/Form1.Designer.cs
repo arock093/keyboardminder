cde00a76a917a971c44396b2e1cab9f6917376d6
arock093 <44488583+arock093@users.noreply.github.com>
2024-09-18 22:11:08 -0600

    Add tool status to form

--- ToolManagerGUI/Form1.Designer.cs
+++ ToolManagerGUI/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status";
+            // 
+            // toolStatusLabel
+            // 
+            this.toolStatusLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.toolStatusLabel.AutoSize = true;
+            this.toolStatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStatusLabel.Location = new System.Drawing.Point(33, 58);
+            this.toolStatusLabel.Name = "toolStatusLabel";
+            this.toolStatusLabel.Size = new System.Drawing.Size(42, 20);
+            this.toolStatusLabel.TabIndex = 1;
+            this.toolStatusLabel.Text = "label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStatusLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label toolStatusLabel;
     }
 }
 
--- ToolManagerGUI/Form1.cs
+++ ToolManagerGUI/Form1.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,24 @@ namespace ToolManagerGUI
         public Form1()
         {
             InitializeComponent();
+            List<string> pnames = new List<string>();
+            var lines = File.ReadLines(@"C:\Users\arock\Documents\ToolManagerConfig.txt");
+            foreach (var line in lines)
+                pnames.Add(line);
+            toolStatusLabel.Text = "";
+            for (int i = 0; i < pnames.Count; i++)
+            {
+                Process[] pname = Process.GetProcessesByName(pnames[i]);
+                if (pname.Length == 0)
+                {
+
+                    toolStatusLabel.Text += pnames[i] + ": Not Running\n";
+                }
+                else
+                {
+                    toolStatusLabel.Text += pnames[i] + ": Running\n";
+                }
+            }
         }
     }
 }
