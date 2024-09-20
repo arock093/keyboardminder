31378a0f271b003572e1e2aa34c47e63a90d5908
arock093 <44488583+arock093@users.noreply.github.com>
2024-09-19 19:51:02 -0600

    Add tool controls

--- ToolManagerGUI/Form1.Designer.cs
+++ ToolManagerGUI/Form1.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.toolStatusLabel = new System.Windows.Forms.Label();
+            this.startRadioButton = new System.Windows.Forms.RadioButton();
+            this.pauseRadioButton = new System.Windows.Forms.RadioButton();
+            this.stopRadioButton = new System.Windows.Forms.RadioButton();
+            this.restartRadioButton = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.durationTextBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -54,15 +61,99 @@
             this.toolStatusLabel.TabIndex = 1;
             this.toolStatusLabel.Text = "label";
             // 
+            // startRadioButton
+            // 
+            this.startRadioButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.startRadioButton.AutoSize = true;
+            this.startRadioButton.Location = new System.Drawing.Point(28, 151);
+            this.startRadioButton.Name = "startRadioButton";
+            this.startRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.startRadioButton.TabIndex = 2;
+            this.startRadioButton.TabStop = true;
+            this.startRadioButton.Text = "Start";
+            this.startRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // pauseRadioButton
+            // 
+            this.pauseRadioButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pauseRadioButton.AutoSize = true;
+            this.pauseRadioButton.Location = new System.Drawing.Point(28, 174);
+            this.pauseRadioButton.Name = "pauseRadioButton";
+            this.pauseRadioButton.Size = new System.Drawing.Size(55, 17);
+            this.pauseRadioButton.TabIndex = 3;
+            this.pauseRadioButton.TabStop = true;
+            this.pauseRadioButton.Text = "Pause";
+            this.pauseRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // stopRadioButton
+            // 
+            this.stopRadioButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.stopRadioButton.AutoSize = true;
+            this.stopRadioButton.Location = new System.Drawing.Point(28, 197);
+            this.stopRadioButton.Name = "stopRadioButton";
+            this.stopRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.stopRadioButton.TabIndex = 4;
+            this.stopRadioButton.TabStop = true;
+            this.stopRadioButton.Text = "Stop";
+            this.stopRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // restartRadioButton
+            // 
+            this.restartRadioButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.restartRadioButton.AutoSize = true;
+            this.restartRadioButton.Location = new System.Drawing.Point(28, 220);
+            this.restartRadioButton.Name = "restartRadioButton";
+            this.restartRadioButton.Size = new System.Drawing.Size(59, 17);
+            this.restartRadioButton.TabIndex = 5;
+            this.restartRadioButton.TabStop = true;
+            this.restartRadioButton.Text = "Restart";
+            this.restartRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(336, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Duration:";
+            // 
+            // durationTextBox
+            // 
+            this.durationTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.durationTextBox.Location = new System.Drawing.Point(399, 176);
+            this.durationTextBox.Name = "durationTextBox";
+            this.durationTextBox.Size = new System.Drawing.Size(100, 20);
+            this.durationTextBox.TabIndex = 7;
+            // 
+            // goButton
+            // 
+            this.goButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.goButton.Location = new System.Drawing.Point(560, 174);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 8;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.durationTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.restartRadioButton);
+            this.Controls.Add(this.stopRadioButton);
+            this.Controls.Add(this.pauseRadioButton);
+            this.Controls.Add(this.startRadioButton);
             this.Controls.Add(this.toolStatusLabel);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ToolManager";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +163,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label toolStatusLabel;
+        private System.Windows.Forms.RadioButton startRadioButton;
+        private System.Windows.Forms.RadioButton pauseRadioButton;
+        private System.Windows.Forms.RadioButton stopRadioButton;
+        private System.Windows.Forms.RadioButton restartRadioButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox durationTextBox;
+        private System.Windows.Forms.Button goButton;
     }
 }
 
--- ToolManagerGUI/Form1.cs
+++ ToolManagerGUI/Form1.cs
@@ -15,16 +15,40 @@ namespace ToolManagerGUI
 {
     public partial class Form1 : Form
     {
+        List<CheckBox> checkBoxes = new List<CheckBox>();
+        int[] running;
         public Form1()
         {
             InitializeComponent();
+
+            startRadioButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            pauseRadioButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            stopRadioButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+            restartRadioButton.CheckedChanged += new EventHandler(radioButtons_CheckedChanged);
+
             List<string> pnames = new List<string>();
-            var lines = File.ReadLines(@"C:\Users\arock\Documents\ToolManagerConfig.txt");
+            var lines = File.ReadLines("ToolManagerConfig.txt");
             foreach (var line in lines)
                 pnames.Add(line);
+            running = new int[pnames.Count];
             toolStatusLabel.Text = "";
+            int y = 154;
             for (int i = 0; i < pnames.Count; i++)
             {
+                CheckBox checkBox1 = new CheckBox();
+                checkBox1.AutoSize = true;
+                checkBox1.Location = new System.Drawing.Point(154, y);
+                checkBox1.Name = "checkBox1";
+                checkBox1.Size = new System.Drawing.Size(80, 17);
+                checkBox1.TabIndex = 0;
+                checkBox1.Anchor = AnchorStyles.None;
+                checkBox1.Text = pnames[i];
+                checkBox1.Enabled = false;
+                checkBox1.UseVisualStyleBackColor = true;
+                Controls.Add(checkBox1);
+                checkBoxes.Add(checkBox1);
+                y += 20;
+
                 Process[] pname = Process.GetProcessesByName(pnames[i]);
                 if (pname.Length == 0)
                 {
@@ -34,6 +58,42 @@ namespace ToolManagerGUI
                 else
                 {
                     toolStatusLabel.Text += pnames[i] + ": Running\n";
+                    running[i] = 1;
+                }
+            }
+        }
+        private void radioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb != null)
+            {
+                if (rb.Checked)
+                {
+                    for (int i = 0; i < checkBoxes.Count; i++)
+                    {
+                        if (rb.Name == "startRadioButton")
+                        {
+                            if (running[i] == 1)
+                                checkBoxes[i].Enabled = false;
+                            else
+                                checkBoxes[i].Enabled = true;
+                        }
+                        else
+                        {
+                            if (running[i] == 1)
+                                checkBoxes[i].Enabled = true;
+                            else
+                                checkBoxes[i].Enabled = false;
+                        }
+                        if (rb.Name == "stopRadioButton" || rb.Name == "restartRadioButton")
+                        {
+                            durationTextBox.Enabled = false;
+                        }
+                        else
+                        {
+                            durationTextBox.Enabled = true;
+                        }
+                    }
                 }
             }
         }
